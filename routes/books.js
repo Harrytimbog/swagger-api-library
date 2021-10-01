ce1218ee5bb384f787383775d98845c2cc14b133
Timilehin Arigbede <Harriemannie@gmail.com>
2021-10-01 20:26:26 +0100

    Code swagger details (YAML)

--- routes/books.js
+++ routes/books.js
@@ -4,6 +4,81 @@ const { nanoid } = require('nanoid');
 
 const idLength = 8;
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Book:
+ *       type: object
+ *       required:
+ *         - title
+ *         - author
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the book
+ *         title:
+ *           type: string
+ *           description: The book title
+ *         author:
+ *           type: string
+ *           description: The book author
+ *       example:
+ *         id: d5fE_asz
+ *         title: The New Turing Omnibus
+ *         author: Alexander K. Dewdney
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Books
+ *   description: The books managing API
+ */
+
+/**
+ * @swagger
+ * /books:
+ *   get:
+ *     summary: Returns the list of all the books
+ *     tags: [Books]
+ *     responses:
+ *       200:
+ *         description: The list of the books
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Book'
+ */
+
+
+/**
+ * @swagger
+ * /books/{id}:
+ *    get:
+ *      summary: Get the book by id
+ *      tags: [Books]
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          schema:
+ *            type: string
+ *          required: true
+ *          description: The book id
+ *      responses:
+ *        200:
+ *          description: The book description by id
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/Book'
+ *        404:
+ *          description: The book was not found
+  */
+
+
 router.get("/", (req, res) => {
   const books = req.app.db.get("books");
 
